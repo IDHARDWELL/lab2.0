67bb8ea35cab4552a5208d7adcd1dd2a86cf5125
IDHARDWELL <masimopostoluk2@gmail.com>
2023-09-24 17:22:46 +0300

    Com1

---
+++ FileName.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double z1;
+	double z2;
+	double m;
+	double pi = atan(1.0) * 4;
+
+	cout << " m= "; cin >> m;
+	z1 = cos(m) + sin(m) + cos(3 * m) + sin(3 * m);
+	z2 = 2 * sqrt(2) * cos(m) * sin((pi / 4) + 2 * m);
+
+	cout << endl;
+	cout << "z1= " << z1 << endl;
+	cout << "z2= " << z2 << endl;
+	cin.get();
+	return 0;
+}
